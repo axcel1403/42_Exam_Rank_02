6ee5788d2842b58bc7511233678c7e829634a8b3
Jordan <axcel1403@hotmail.com>
2024-03-07 02:52:41 -0500

    rostring

---
+++ level_3/rostring/rostring.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int i = 0, last = 0, end = 0, flag = 0;
+
+	if (argc >= 2)
+	{
+		while (argv[1][i] && (argv[1][i] == ' ' || argv[1][i] == '\t'))
+			i++;
+		last = i;
+		while (argv[1][i] && (!(argv[1][i] == ' ' || argv[1][i] == '\t')))
+			i++;
+		end = i;
+		while (argv[1][i] && (argv[1][i] == ' ' || argv[1][i] == '\t'))
+			i++;
+		if (argv[1][i])
+			flag = 1;
+		while (argv[1][i])
+		{
+			if (argv[1][i] == ' ' || argv[1][i] == '\t')
+			{
+				write(1, &argv[1][i], 1);
+				while (argv[1][i] && (argv[1][i] == ' ' || argv[1][i] == '\t'))
+					i++;
+				i--;
+			}
+			else
+				write(1, &argv[1][i], 1);
+			i++;
+		}
+		if ((last != 0 && (argv[1][0] != ' ' && argv[1][0] != '\t')) || (flag == 1 && (argv[1][i - 1] != ' ' && argv[1][i - 1] != '\t')))
+			write(1, " ", 1);
+		while (last < end)
+		{
+			write(1, &argv[1][last], 1);
+			last++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
